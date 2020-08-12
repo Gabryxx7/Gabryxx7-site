6f51a828ce49be7b4d1f890832c2bffa304f7890
Gabriele Marini <giaga7@gmail.com>
2020-08-12 17:57:03 +1000

    Trying to fix auto dark mode

--- #jekyll-theme-hydejack/_includes/scripts/dark-mode-fix.js
+++ #jekyll-theme-hydejack/_includes/scripts/dark-mode-fix.js
@@ -2,7 +2,7 @@
   var LM = 'light-mode';
   var DM = 'dark-mode';
   var h = new Date().getHours();
-  if ('matchMedia' in window && window.matchMedia('(prefers-color-scheme)')) return;
+  if (window.match_os  && 'matchMedia' in window && window.matchMedia('(prefers-color-scheme)')) return;
   var m = h <= window._sunrise || h >= window._sunset  ? DM : LM; 
   var n = m === DM ? LM : DM;
   document.body.classList.add(m);
