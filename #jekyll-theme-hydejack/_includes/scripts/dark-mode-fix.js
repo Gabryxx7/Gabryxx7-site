ca21b3755b3d3ff9f5a5b0e7673bf6ed48b83253
Gabriele Marini <giaga7@gmail.com>
2020-08-12 23:26:14 +1000

    Fixed time-based dark mode

--- #jekyll-theme-hydejack/_includes/scripts/dark-mode-fix.js
+++ #jekyll-theme-hydejack/_includes/scripts/dark-mode-fix.js
@@ -2,6 +2,9 @@
   var LM = 'light-mode';
   var DM = 'dark-mode';
   var h = new Date().getHours();
+  document.body.classList.add(DM); /*Start in dark mode and eventually switch */
+  console.log("match_os " + match_os + " time " +h);
+  /* if match_os is false, the time has the preference on deciding  which mode */
   if (window.match_os  && 'matchMedia' in window && window.matchMedia('(prefers-color-scheme)')) return;
   var m = h <= window._sunrise || h >= window._sunset  ? DM : LM; 
   var n = m === DM ? LM : DM;
